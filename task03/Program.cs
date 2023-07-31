3746cdcee1efa8f48f79bc9168da594bb4dd6e0d
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-31 13:57:42 +0300

    Modified third task

--- task03/Program.cs
+++ task03/Program.cs
@@ -12,20 +12,32 @@ int GetNumber(string text) // Функция запрашивает у поль�
     return int.Parse(Console.ReadLine()!);
 }
 
-int[,,] FillArray(int lines, int columns, int depth) //Функция заполняет массив случайными целыми числами от 0 до 99
+int[,,] FillArray(int lines, int columns, int depth) //Функция заполняет трехмерный массив случайными неповторяющемися целыми числами от 0 до 999
 {
     int[,,] array = new int[lines, columns, depth];
+    int[] arrayLine = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
     Random random = new Random();
+    for (int i = 0; i < arrayLine.Length; i++)
+    {
+        arrayLine[i] = random.Next(0, 1000);
+        if (i != 0)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (arrayLine[j] == arrayLine[i])
+                {
+                    arrayLine[j] = random.Next(0, 1000);
+                }
+            }
+        }
+    }
+    int count = 0;
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                    array[i, j, k] = random.Next(0, 100);
-                    if(k!=0)
-                    {
-                    while (array[i, j, k]==array[i,j,k-1])
-                    array[i, j, k] = random.Next(0, 100);
-                    }
+                array[i, j, k] = arrayLine[count];
+                count++;
             }
 
     return array;
@@ -49,6 +61,10 @@ void PrintArray(int[,,] array) //Функция печатает массив
 int[,,] mainArray = FillArray(GetNumber("Введите глубину:"),
                            GetNumber("Введите количество строк:"),
                            GetNumber("Введите столбцов:"));
-
-PrintArray(mainArray);
-Console.WriteLine();
+if ((mainArray.GetLength(0) * mainArray.GetLength(1) * mainArray.GetLength(2)) > 999)
+    Console.WriteLine("Количество элементов массива более 999, уменьшите количество строк, столбцов или глубину");
+else
+{
+    PrintArray(mainArray);
+    Console.WriteLine();
+}
